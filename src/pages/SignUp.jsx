a6d0b5a426bdd4451e5c5778c8c11dce16ea8526
Mihalj97 <mihalj.tandi5@gmail.com>
2022-02-10 20:06:11 +0100

    Google OAuth

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -6,6 +6,7 @@ import { db } from '../firebase.config'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibillityIcon from '../assets/svg/visibilityIcon.svg'
 import { toast } from 'react-toastify';
+import OAuth from '../components/OAuth'
 
 
 function SingUp() {
@@ -115,7 +116,7 @@ function SingUp() {
               </button>
             </div>
           </form>
-          {/* Google OAuth */}
+          <OAuth />
           <Link to='/sign-in' className='registerLink'>
             Sign In Instead
           </Link>
